0b92ab0d2087285aea02ae0f736638e035ad702f
Lana Black <lanablack@amok.cc>
2018-07-22 19:13:46 +0000

    IPv6 parsing and showing.

--- src/Data/IP/Types.hs
+++ src/Data/IP/Types.hs
@@ -64,6 +64,9 @@ ip4 = do ns <- word32 `sepBy1'` char '.'
     word32 = 0 <$ char '0' <|> toW8 <$> oneOf ['1' .. '9'] <*> many digit
     toW8 n ns = foldl' (\x y -> x * 10 + y) 0 . map (fromIntegral . digitToInt) $ n : ns
 
+instance Show IPv6 where
+  show ip = ByteString.unpack $ showIPv6 ip
+
 showIPv6 :: IPv6 -> ByteString
 showIPv6 ip
     -- IPv4-mapped IPv6 address
@@ -71,7 +74,7 @@ showIPv6 ip
     -- IPv4-Compatible IPv6 Address (exclude IPRange ::/112)
     | all (== 0) [w0,w1,w2,w3,w4,w5] && w6 >= 1 = "::" <> showIPv4 (IPv4 $ fromHiAndLo w6 w7)
     -- IPv6 with ::
-    | end - begin > 1 = traceShow [begin,end,diff] $ showFields prefix <> "::" <> showFields suffix
+    | end - begin > 1 = showFields prefix <> "::" <> showFields suffix
     -- IPv6 without ::
     | otherwise = showFields fields
   where
@@ -83,34 +86,41 @@ showIPv6 ip
         scanl (\c i -> if i == 0 then c - 1 else 0) 0 fields `zip` [0..]
     showFields = mconcat . intersperse ":" . map (\f -> ByteString.pack $ showHex f "")
 
-
-{-
-showIPv6 :: IPv6 -> ShowS
-showIPv6 ip@(IP6 (a1,a2,a3,a4))
-  -- IPv4-Mapped IPv6 Address
-  | a1 == 0 && a2 == 0 && a3 == 0xffff =
-      showString "::ffff:" . showIPv4 (IP4 a4)
-  -- IPv4-Compatible IPv6 Address (exclude IPRange ::/112)
-  | a1 == 0 && a2 == 0 && a3 == 0 && a4 >= 0x10000 =
-      showString "::" . showIPv4 (IP4 a4)
-  -- length of longest run > 1, replace it with "::"
-  | end - begin > 1 =
-      showFields prefix . showString "::" . showFields suffix
-  -- length of longest run <= 1, don't use "::"
-  | otherwise =
-      showFields fields
-  where
-    fields = fromIPv6 ip
-    showFields = foldr (.) id . intersperse (showChar ':') . map showHex
-    prefix = take begin fields  -- fields before "::"
-    suffix = drop end fields    -- fields after "::"
-    begin = end + diff          -- the longest run of zeros
-    (diff, end) = minimum $
-        scanl (\c i -> if i == 0 then c - 1 else 0) 0 fields `zip` [0..]
--}
+instance Read IPv6 where
+  readsPrec _ s = let bs = ByteString.pack s
+                  in case parse ip6 bs of
+                       Fail _ _ _ -> []
+                       Done rest ip -> [(ip, ByteString.unpack rest)]
 
 ip6 :: Parser IPv6
-ip6 = undefined
+ip6 = ip4embedded
+      -- Matches ::1
+  <|> do _ <- colons
+         bs <- hexcolon
+         let l = length bs
+         traceShowM bs
+         when (l > 7) $
+           fail "Ipv6 address too long"
+         pure (wordsToIP6 $ take (8 - l) (repeat 0) ++ bs)
+      -- Matches 2001:db8::1
+  <|> do pref <- hexcolon
+         _ <- colons
+         suf <- hexcolon
+         let l = length pref + length suf
+         when (l > 7) $
+           fail "Invalid IPv6"
+         pure $ wordsToIP6 $ pref ++ take (8 - l) (repeat 0) ++ suf
+      -- Matches 2001:db8::
+  <|> do pref <- hexcolon
+         _ <- colons
+         let l = length pref
+         when (l > 7) $
+           fail "Invalid IPv6"
+         pure $ wordsToIP6 $ pref ++ take (8 - l) (repeat 0)
+  <|> do bs <- hexcolon
+         when (length bs /= 8) $
+           fail "Invalid IPv6"
+         pure $ wordsToIP6 bs
   where
     colons = void $ string "::"
     hex = do n <- hexadecimal :: Parser Word
@@ -118,72 +128,39 @@ ip6 = undefined
                fail "hex is too large"
              pure (fromIntegral n :: Word16)
     hexcolon = hex `sepBy1'` char ':'
-
     ip4embedded = -- Matches ::ffff:192.168.0.1
-                  do colons
+                  -- XXX: Check that bs here is equal to ::ffff: ?
+                  do _ <- colons
                      bs <- beforeEmbedded
                      emb <- splitIP4 <$> ip4
                      pure (wordsToIP6 $ bs ++ emb)
-                  -- Matches full IPv6
-              <|> do rs <- hexcolon
-                     when (length rs /= 8) $
+                  -- Matches 2001:db8::192.168.0.1
+              <|> do bs <- beforeEmbedded
+                     _ <- colons
+                     emb <- splitIP4 <$> ip4
+                     when (length bs > 6) $
                        fail "Invalid address length"
-                     pure (wordsToIP6 rs)
+                     pure (wordsToIP6 $ bs ++ emb)
+              <|> do bs <- beforeEmbedded
+                     when (length bs /= 6) $
+                       fail "Invalid address"
+                     emb <- splitIP4 <$> ip4
+                     pure (wordsToIP6 $ bs ++ emb)
     beforeEmbedded = many (hex <* char ':')
 
 splitIP4 :: IPv4 -> [Word16]
 splitIP4 (IPv4 a) = [hiWord a, loWord a]
 
 wordsToIP6 :: [Word16] -> IPv6
-wordsToIP6 ws | length ws > 8 = error "wordsToIP6: invalid list length"
-              | otherwise = IPv6 $ go (take 8 $ ws ++ repeat 0)
+wordsToIP6 ws | length ws /= 8 = error "wordsToIP6: invalid list length"
+              | otherwise = IPv6 $ go ws
   where
-    go = foldr (\w r -> r * 0x10000 + fromIntegral w) 0
+    go = foldl' (\r w -> r * 0x10000 + fromIntegral w) 0
 
 ip6ToWords :: IPv6 -> [Word16]
-ip6ToWords (IPv6 a) = reverse $ foldr (\n r -> fromIntegral (a .&. (0xffff `shift` 16 * n)) : r) [] [0..7]
-
-{-
-ip6' :: Parser [Int]
-ip6' = ip4Embedded
-   <|> do colon2
-          bs <- option [] hexcolon
-          format [] bs
-   <|> try (do rs <- hexcolon
-               check rs
-               return rs)
-   <|> do bs1 <- hexcolon2
-          bs2 <- option [] hexcolon
-          format bs1 bs2
-  where
-    hexcolon = hex `sepBy1` char ':'
-    hexcolon2 = manyTill (hex <* char ':') (char ':')
-    check bs = when (length bs /= 8) (fail "IPv6 address4")
-
-ip4Embedded :: Parser [Int]
-ip4Embedded = try (do colon2
-                      bs <- beforeEmbedded
-                      embedded <- ip4'
-                      format [] (bs ++ ip4ToIp6 embedded))
-              -- matches 2001:db8::192.0.2.1
-          <|> try (do bs1 <- manyTill (try $ hex <* char ':') (char ':')
-                      bs2 <- option [] beforeEmbedded
-                      embedded <- ip4'
-                      format bs1 (bs2 ++ ip4ToIp6 embedded))
-              -- matches 2001:db8:11e:c00:aa:bb:192.0.2.1
-          <|> try (do bs <- beforeEmbedded
-                      embedded <- ip4'
-                      let rs = bs ++ ip4ToIp6 embedded
-                      check rs
-                      return rs)
-  where
-    beforeEmbedded = many $ try $ hex <* char ':'
-    ip4ToIp6 [a,b,c,d] = [ a `shiftL` 8 .|. b
-                         , c `shiftL` 8 .|. d ]
-    ip4ToIp6 _ = error "ip4ToIp6"
-    check bs = when (length bs /= 8) (fail "IPv6 address4")
--}
-
+ip6ToWords (IPv6 a) = reverse $ foldr f [] [0..7]
+  where s = (16 *)
+        f n r = fromIntegral ((a .&. (0xffff `shift` s n)) `shiftR` s n) : r
 
 oneOf :: [Char] -> Parser Char
 oneOf xs = satisfy (`elem` xs)
